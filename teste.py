a985cb3866cf00e8b0c8bf8bcbe1c81b302ec752
Guilherme Roth Cassol <129892129+guicassol8@users.noreply.github.com>
2024-10-04 16:23:11 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,24 +1,49 @@
 import nltk
-import unicodedata
 from nltk.corpus import words
-
-def rem(texto):
-    # Normaliza o texto e remove os acentos
-    return ''.join(
-        c for c in unicodedata.normalize('NFD', texto)
-        if unicodedata.category(c) != 'Mn'
-    )
+import random
 
 # Carregue a lista de palavras
 word_list = words.words()
 
 # Filtrar palavras com até 5 letras
-short_words = [word for word in word_list if len(word) == 5 and word.isalpha() and not word[0].isupper()]
+short_words = [word for word in word_list if len(word) == 5 and word.isalpha() and not word[0].istitle()]
 
-word_list = list(set(word_list))
+word_list = list(set(short_words))
 
 word_list.sort()
+
+palavras = dict()
+
+def verificarRepeticao(palavra):
+    for i in range(len(palavra)):
+        if palavra[i] in palavra[(i+1):]:
+            return True
+    return False
+
+
+def verificarComum():
+    for word in word_list:
+        for letter in word:
+            if letter not in palavras:
+                palavras[letter] = 0
+            palavras[letter] += 1
+    maiorPalavra = ""
+    maiorQuantidade = 0
+    for word in word_list:
+        if verificarRepeticao(word):
+            continue
+        quantidadeAtual = 0
+        for letter in word:
+            if letter in palavras:
+                quantidadeAtual += palavras[letter]
+        if quantidadeAtual > maiorQuantidade:
+            maiorQuantidade = quantidadeAtual
+            maiorPalavra = word
+
+    return maiorPalavra
 
 # Exibir as palavras
 print(short_words)
 print(len(short_words))
+print(f"Primeira palavra: {word_list[random.randint(0, len(short_words) - 1)]}")
+print(verificarComum())
