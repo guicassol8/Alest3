d0d6efc5d2c7fe9466e5f6e24dc20ea482dd13c9
Guilherme Roth Cassol <129892129+guicassol8@users.noreply.github.com>
2024-10-04 16:22:57 -0300

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -1,17 +1,70 @@
 import os
 import unicodedata
+import nltk
+#nltk.download('words')
+from nltk.corpus import words
+import random
+
+word_list = words.words()
+descart_list = set()
+for word in open("descarte.txt"):
+    descart_list.add(word.strip())
+
+short_words = [word.strip() for word in word_list if len(word) == 5 and word.isalpha() and not word[0].istitle() and word.strip() not in descart_list]
+
+word_list = list(set(short_words))
+for word in open("adicao.txt"):
+    word_list.append(word)
 
 
-def remover_acento(texto):
-    return ''.join(
-        c for c in unicodedata.normalize('NFD', texto)
-        if unicodedata.category(c) != 'Mn'
-    )
+listaOriginal = word_list.copy()
 
 os.system("cls")
 
-listaPalavras = [linha.strip() for linha in open("palavras.txt", "r", encoding = "utf-8")]
-listaPalavras = [remover_acento(linha) for linha in listaPalavras]
+valorLetras = dict()
+# Quantidade de repeticao de letras
+def dicionarioRepeticao():
+    for word in word_list:
+        for letter in word:
+            if letter not in valorLetras:
+                valorLetras[letter] = 0
+            valorLetras[letter] += 1
+dicionarioRepeticao()
+
+def verificarRepeticao(palavra):
+    for i in range(len(palavra)):
+        if palavra[i] in palavra[(i+1):]:
+            return True
+    return False
+
+def valorPalavra(palavra):
+    valorTotal = 0
+    if verificarRepeticao(palavra):
+        return 0
+    for letra in palavra:
+        if letra in valorLetras:
+            valorTotal += valorLetras[letra]
+    return -valorTotal
+
+def valorPalavraSemRepeticao(palavra):
+    valorTotal = 0
+    for letra in palavra:
+        if letra in valorLetras:
+            valorTotal += valorLetras[letra]
+    return -valorTotal
+
+def melhorChute():
+    for palavra in listaOriginal:
+        melhorPalavra = True
+        for letra in palavra:
+            if letra in posicaoCorreta.values() or letra in letraCorreta:
+                melhorPalavra = False
+                break
+        if melhorPalavra:
+            return palavra
+
+word_list.sort()
+word_list = sorted(word_list, key=valorPalavra)
 
 # Chave index, Conteudo letra
 posicaoCorreta = dict()
@@ -21,6 +74,12 @@ letraCorreta = dict()
 
 # Chave index, Conteudo lista de letras que nao estao ali
 aliNao = dict()
+
+# Caso esteja preenchido significa que o numero de letras eh aquele 100%
+quantAbsoluta = set()
+
+# Palavra com maior quantidade de letras comumm sem repeticoes
+firstGuess = "arose"
 
 # Lista de possíveis palavras
 possiveisPalavras = []
@@ -35,30 +94,39 @@ def checarPalavra(palavra):
 
     # Chave letra, Conteudo quantidade de aparicoes
     letrasAtuais = dict()
+    i = 0
     for letra in palavra:
         if letra not in letrasAtuais:
             letrasAtuais[letra] = 0
         letrasAtuais[letra] += 1
+        if i in aliNao:
+            if letra in aliNao[i]:
+                print("false 1")
+                return False
+        i += 1
 
     for letra in letraCorreta:
         if letra in letrasAtuais:
-            if letrasAtuais[letra] != letraCorreta[letra]:
+            if letra in quantAbsoluta:
+                if letrasAtuais[letra] != letraCorreta[letra]:
+                    print("false 4")
+                    return False
+            elif letrasAtuais[letra] < letraCorreta[letra]:
+                print(letrasAtuais[letra] < letraCorreta[letra])
+                print(letraCorreta[letra] == 0)
+                print(letra)
                 print("false 2")
                 return False
         else:
             if letraCorreta[letra] == 0:
                 continue
-            if palavra == "terno":
-                print(f"letraCorreta[letra]: {letraCorreta[letra]}")
-                print(f"letra: {letra}")
-                print(f"letrasAtuais[letra]: {letrasAtuais[letra]}")
-                print("false 3")
+            print("false 3")
             return False
     return True
 
 
 while True:
-    escolha = input("1 - AddPosCorreta 2 - AddLetraCorreta 3 - Testar - 4 Sair\n")
+    escolha = input("1 - AddPosCorreta 2 - AddLetraCorreta 3 - Testar - 4 Sair 5 - AliNao 6 - Randomize 7 - QuantAbsoluta\n")
     if escolha == "1":
         index = int(input("Digite o index\n"))
         letra = input("Digite a letra\n")
@@ -67,12 +135,61 @@ while True:
         letra = input("Digite a letra\n")
         quantidade = int(input("Quantidade\n"))
         letraCorreta[letra] = quantidade
+        if quantidade == 0:
+            quantAbsoluta.add(letra)
     if escolha == "3":
-        for palavra in listaPalavras:
+        for palavra in word_list:
             if checarPalavra(palavra):
                 possiveisPalavras.append(palavra)
-        listaPalavras = possiveisPalavras.copy()
+        word_list = possiveisPalavras.copy()
         possiveisPalavras.clear()
-        print(f"Palavra possivel: {listaPalavras}")
+        valorLetras.clear()
+        dicionarioRepeticao()
+        word_list = sorted(word_list, key=valorPalavra)
+        listaOriginal = sorted(listaOriginal, key=valorPalavra)
+        print(f"Palavras possiveis: {word_list}")
+        print(melhorChute())
+    if escolha == "5":
+        index = int(input("Digite o index\n"))
+        letra = input("Digite a letra\n")
+        if index not in aliNao:
+            aliNao[index] = []
+        aliNao[index].append(letra)
     if escolha == "4":
         break
+    if escolha == "6":
+        print(word_list[random.randint(0, len(word_list) - 1)])
+        print(valorLetras)
+    if escolha == "7":
+        letra = input("Digite a letra\n")
+        quantAbsoluta.add(letra)
+
+#unit
+
+# Mais um caso possivel eh quando tu tem certeza que aquela letra eh aquilo e pronto ai substituiria o or letraCorreta[letra] == 0 por um certeza, deixando ainda mais especifico
+
+#Informacoes possives:
+# - Posicao correta
+# - Posicao incorreta
+# - Quantas letras de um tipo tem
+# - Caso tenha usado mais de uma vez, consegue garantir quantidade, caso seja 0 tambem garante
+# - Fazer tambem funcao que caso chegue em um caso e nao achou, que faca por forca bruta ate acertar, caso nao acerte so pegue oq tem ali
+# - Caso coloque duas letras iguais, e apenas uma fique acizentada, eu sei que a posicao tambem n eh na do acinzentado
+# - Outra coisa que podia ser mais pra frente, é ordenar com o criterio de letras mais comuns, assim seria uma priority queue com o criterio sendo maior quantidade de letras comuns
+#   - Por enquanto ta funcionado bem, mas caso queira postar isso no github vou também fazer questão de usar uma heap, já que po tem que mostra que sabe usar
+#
+# - Vejo duas estrategias possiveis, pegar sempre o primeiro da lista com maior quantidade de repeticao de letras sem repeticao ou maior repeticao em um geral
+# - A segunda estrategia tem potencial de eliminar muitas que repetem a mesma letra, mas a grande questao eh que caso seja um caso com letras repetidas geralmente o mesmo problema
+# vai se repetir, onde existem muitas palavras quase identicas e nao tem oque fazer, acho que a primeira eh melhor por eliminar mais opcoes no geral, caso a palavra seja muito repetida
+# e tenha outras que sao quase iguais, ambas estrategias vao ter o mesmo problema, a segunda eh muito situacional e tem um ganho muuito menor comparado ao primeiro, vamos ficar com o pri
+# Refazer o valor das letras pra quando fazer o sort se basear no novo valor atual, vai ajudar a pegar a pelavra que elimina mais outras
+# 
+# Na real uma estrategia muito interessante, eh pegar 3 palavras com as letras mais comuns que tem no alfabeto e usar as 3, dessa maneira vai ser possivel recolher muitas informacoes
+# pq caso chute a primeira e parta dela, sempre tem uma letra (caso acerte algo) que vai estar sendo reutilizada desnecessariamente, no lugar de uma letra que poderia dar uma info nova
+# Estrategia que acho AINDA melhor eh uma melhora dessa, eu vou dar o sort denovo no valor das palavras com as informacoes pegas do primeiro (arose) e a partir dai eu vou pegar uma pala
+# vra que esta de acordo com o novo dicionario, mas nao repete nenhuma letra de arose, assim vou maximizar ainda mais as informacoes, pegando o valor relativo ao dicionario atual
+#
+# Depois alem da repeticao da pra fazer uma analise de posicoes mais comuns das letras, isso com certeza seria util
+#
+# Depois que integrar com site da pra testar varias, mas atualmente oque acredito que faz mais sentido eh, dar 3 chutes que nao repetem letras, mas esses chutes vem da palavra com
+# maior repeticao de letras, mas baseado nas descobertas, assim a chance aumenta ainda mais de cortar casos corretos
